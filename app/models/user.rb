08c3d666a1dba6c0d98096ccaa6fb6d6120bffc2
Rei <reiacewakaba310@gmail.com>
2023-05-26 16:57:37 +0900

    バリデーションとエラーメッセージの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
+  validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :image,        presence: true
+  
   has_one_attached :image
 end
