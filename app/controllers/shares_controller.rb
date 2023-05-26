4f14c0df377489b38fdf134c5bf69707fe975bb9
Rei <reiacewakaba310@gmail.com>
2023-05-26 14:54:43 +0900

    ActiveStorageの導入

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,2 +1,7 @@
 class SharesController < ApplicationController
+
+    # usersテーブルにActiveStorageを使うための
+    # def message_params
+    #     params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+    # end
 end
