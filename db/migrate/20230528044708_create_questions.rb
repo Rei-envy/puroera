b2b381dc8769eb95c4b2842fd91dfcf6c7d8ba7f
Rei <reiacewakaba310@gmail.com>
2023-05-28 13:52:55 +0900

    questionモデルとアソシエーションの設定

--- db/migrate/20230528044708_create_questions.rb
+++ db/migrate/20230528044708_create_questions.rb
@@ -1,10 +1,10 @@
-class CreateShares < ActiveRecord::Migration[6.0]
+class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
-    create_table :shares do |t|
+    create_table :questions do |t|
       t.string     :title,          null: false, default: ""
       t.integer    :category_id,    null: false
-      t.text       :guess,          null: false 
-      t.text       :solution,       null: false
+      t.text       :hypothesis,     null: false 
+      t.text       :action
       t.text       :thought
       t.references :user,           null: false, foreign_key: true
       t.timestamps
