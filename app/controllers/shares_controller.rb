929e708c4dc18c9068d9debf6dae80686b9cc68b
Rei <reiacewakaba310@gmail.com>
2023-05-28 11:46:17 +0900

    投稿機能の追加

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -1,6 +1,26 @@
 class SharesController < ApplicationController
+    before_action :authenticate_user!, only: [:new]
     def index
         @shares = Share.order("created_at DESC")
+    end
+
+    def new
+        @share = Share.new
+    end
+
+    def create
+        @share = Share.new(share_params)
+        if @share.save
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    private
+
+    def share_params
+        params.require(:share).permit(:title, :category_id, :guess, :solution, :thought).merge(user_id: current_user.id)
     end
 
     # usersテーブルにActiveStorageを使うための
