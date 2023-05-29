a4d828717b26219a4a3a4c78e951a19354570fb8
Rei <reiacewakaba310@gmail.com>
2023-05-29 17:45:13 +0900

    answerの単体テストコードの記述

--- spec/factories/answers.rb
+++ spec/factories/answers.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :answer do
-    
+    answer                 { 'こうしたらいいよ' }
   end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @question = FactoryBot.create(:question,user_id: @user.id)
+    @answer = FactoryBot.build(:answer,user_id: @user.id, question_id: @question.id)
+  end
+
+  describe '回答の保存' do
+    context '回答の保存ができる場合' do
+      it '回答に関わる全ての項目が存在すれば保存できる' do
+        expect(@answer).to be_valid
+      end
+    end
+
+    context '回答の保存ができない場合' do
+      it 'answerが空では登録できない' do
+        @answer.answer = ''
+        @answer.valid?
+        expect(@answer.errors.full_messages).to include "Answer can't be blank"
+      end
+      it 'ユーザー情報がない場合は登録できない' do
+        @answer.user = nil
+        @answer.valid?
+        expect(@answer.errors.full_messages).to include 'User must exist'
+      end
+      it '投稿の情報がない場合は登録できない' do
+        @answer.question = nil
+        @answer.valid?
+        expect(@answer.errors.full_messages).to include 'Question must exist'
+      end
+    end
+  end
 end
