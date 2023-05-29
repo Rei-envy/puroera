78a1c30258ddce166533d17164b519e0a65ff602
Rei <reiacewakaba310@gmail.com>
2023-05-29 13:22:41 +0900

    一覧表示機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :shares do
     resources :comments, only: :create
   end
-  resources :questions, only: [:index, :new, :create]
+  resources :questions
 end
