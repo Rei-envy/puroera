43f8be01e9df80781d16b3be9458a127f5374b78
Rei <reiacewakaba310@gmail.com>
2023-05-29 18:30:53 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,7 @@ Rails.application.routes.draw do
   resources :questions do
     resources :answers, only: :create
   end
-  resources :rooms, only: [:index, :new, :create, :destroy]
+  resources :rooms, only: [:index, :new, :create, :destroy] do
+    resources :messages, only: [:index, :create]
+  end
 end
