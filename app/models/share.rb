b146c23d2eab705edba05e3747f74f6065c1a698
Rei <reiacewakaba310@gmail.com>
2023-06-02 17:46:42 +0900

    削除の際のエラーの解消
    子要素も消せるようにしていなかったため、,dependent: :destroyを追記した

--- app/models/share.rb
+++ app/models/share.rb
@@ -1,6 +1,6 @@
 class Share < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments,dependent: :destroy
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
 
